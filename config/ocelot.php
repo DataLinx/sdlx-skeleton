a8cdfab7cf612efa9d6e92b55eaa6a55d6b367fc
Omer Sabic <omer@datalinx.si>
2020-12-24 08:14:10 +0100

    OC-71 Implement icon Blade component with Font Awesome

---
+++ config/ocelot.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Font Awesome kit URL
+    |--------------------------------------------------------------------------
+    | Create your FA kit and put the URL in the .env file
+    */
+    'fontawesome_kit_url' => env('FONTAWESOME_KIT_URL', 'https://kit.fontawesome.com/94d9db3525.js'),
+];
