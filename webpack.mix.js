4775992f664094dd4e52ebf58ec05fe5b186b189
Omer Sabic <omer@datalinx.si>
2020-12-30 12:49:22 +0100

    OC-5 Move JS/CSS assets to core, update Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,12 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix .js([
+mix .setPublicPath('public')
+    .js([
+        'vendor/ocelot/core/public/js/core.js',
         'resources/js/app.js',
-        'vendor/ocelot/core/resources/js/core.js',
-    ], 'public/js')
+    ], 'js/app.js')
     .extract()
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('vendor/ocelot/core/resources/sass/core.scss', 'public/css')
-    .browserSync(process.env.APP_URL)
 
 if (mix.inProduction()) {
     mix.version()
